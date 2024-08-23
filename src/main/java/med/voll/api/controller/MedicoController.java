eb62363c4740801028a2c4d0f932768d0957a139
Matheus H <matheus.hsilvaa18@gmail.com>
2024-08-22 23:05:45 -0300

    feat: creating a route to bring all medical records and working with pagination

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,8 +27,13 @@ public class MedicoController {
     private MedicoRepository repository;
     
     @PostMapping
-    @Transactional
+    @Transactional // Transação ativa com o banco de dados, porque vamos persistir dados no banco.
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+    
+    @GetMapping
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = {"email"}) Pageable paginacao /*Trabalhando com paginacao*/) {
+    	return repository.findAll(paginacao).map(DadosListagemMedicos::new);
     }
 }
